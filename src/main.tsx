9f9e8d0796da5b991662dce95c4752661f2bd7ce
Mohammed Ismail Najeeb <mohammed.najeeb@volantetech.com>
2025-10-03 17:18:16 +0530

    update versions

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-
-ReactDOM.render(
+const root = createRoot(document.getElementById("root")!);
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
